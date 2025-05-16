f1131d0b3c2d146bafbf2bc3352e16b293690a7e
Erion Jasiqi <erion_jasiqi@sluz.ch>
2025-05-16 15:09:07 +0200

    changed the order in the conversionTable to go from small to big and added furlong

--- src/conversion.ts
+++ src/conversion.ts
@@ -1,12 +1,12 @@
 export const conversionTable = {
-  league: [3, "miles"],
-  mile: [1760, "yard"],
-  // TODO: introduce "furlong" (as 22 yards; equal to 201.168 meters)
-  yard: [3, "foot"],
-  foot: [12, "inch"],
-  inch: [2.54, "centimeter"],
   centimeter: [0.01, "meter"],
   meter: [1, "meter"],
+  inch: [2.54, "centimeter"],
+  foot: [12, "inch"],
+  yard: [3, "foot"],
+  furlong: [220, "yard"],
+  mile: [1760, "yard"],
+  league: [3, "miles"],
 };
 
 export function convertToMeters(value: number, unit: string): number {
